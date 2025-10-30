474769b942eb692f95342937737c6b4cdce9eaf1
Esteban Bustamante <esbon1253@gmail.com>
2025-10-30 01:03:43 -0300

    Add absolute path in Makefiles. Add lib_fxp.h for easy including. Add test program

---
+++ code/fxp/lib_fxp.h
@@ -0,0 +1,10 @@
+//HEADER FILE to be included in source files
+//author: Esteban Bustamante <ebustamante896@gmail.com>
+//DATE: 2025-10-29
+
+#include "ulongy.h" 
+#include "fxp_l.h" 
+#include "fxp_aux.h" 
+#include "print_as_bits.h" 
+#include "fxp.h" 
+#include "fxp_conv.h"
---
+++ code/src/test_fxp/test_fxp.c
@@ -0,0 +1,10 @@
+#include "lib_fxp.h"
+#include <stdio.h>
+
+int main()
+{
+  //  n1 = fxp_bin(5,4);
+  
+  printf("Default N_INT: %d",  fxp_get_whole_bits());
+  return 0;
+}
